3b3e0dbb7f372539ab555772e840477988627e84
mishkasniper <mikhail.al.zakharov@gmail.com>
2026-02-26 00:12:00 +0300

    реализован вывод дерева, на которое указывает последний коммит

--- 20260216/1/prog.py
+++ 20260216/1/prog.py
@@ -41,10 +41,31 @@ def main():
         commit_hash = branch_file.read_bytes().decode().strip()
         obj_path = repo / "objects" / commit_hash[:2] / commit_hash[2:]
         header, _, body = zlib.decompress(obj_path.read_bytes()).partition(b'\x00')
-        kind, size = header.split()
 
-        out = body.decode()
-        print(out)
+        lines = body.decode().split('\n')
+
+        tree_hash = ''
+        for line in lines:
+            if line.startswith('tree '):
+                tree_hash = line[5:].strip()
+                break
+
+        obj_path = repo / "objects" / tree_hash[:2] / tree_hash[2:]
+        header, _, tree_body = zlib.decompress(obj_path.read_bytes()).partition(b'\x00')
+
+        while tree_body:
+            treeobj, _, tree_body = tree_body.partition(b'\x00')
+            tmode, tname = treeobj.split()
+            num, tree_body = tree_body[:20], tree_body[20:]
+            mode_str = tmode.decode()
+            if mode_str.startswith('04') or mode_str == '40000':
+                obj_type = 'tree'
+            else:
+                obj_type = 'blob'
+            print(f"{obj_type} {num.hex()}    {tname.decode()}")
+
+        
+        
         
 
 if __name__ == "__main__":
