3d0d59429d255e7ab433a0da2a573f47017a2a4f
mishkasniper <mikhail.al.zakharov@gmail.com>
2026-03-15 03:25:59 +0300

    start work with new hm

---
+++ 20260312/1/prog.py
@@ -0,0 +1,196 @@
+from shlex import split
+from cowsay import cowsay, list_cows, read_dot_cow
+from io import StringIO
+
+
+
+class Position:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+    
+    def __eq__(self, other):
+        if not isinstance(other, Position):
+            return False
+        return self.x == other.x and self.y == other.y
+    
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+    
+    def move(self, dx: int, dy: int) -> None:
+        self.x = (self.x + dx) % 10
+        self.y = (self.y + dy) % 10
+    
+    def copy(self):
+        return Position(self.x, self.y)
+    
+    def __getitem__(self, index):
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError("Position index out of range")
+        
+    def __setitem__(self, index, value):
+        if index == 0:
+            self.x = value % 10
+        elif index == 1:
+            self.y = value % 10
+        else:
+            raise IndexError("Position index out of range")
+    
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+class Monster:
+    def __init__(self, pos: Position, name: str, phrase: str, hp: int):
+        self.pos = pos
+        self.name = name
+        self.phrase = phrase
+        self.hp = hp
+
+commands = ["up", "down", "left", "right", "addmon"]
+player = Position(0, 0)
+monsters = []
+
+jgsbat = read_dot_cow(StringIO("""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\\\\\\\--//|.'-._  (
+     )'   .'\/o\/o\/'.   `(
+      ) .' . \====/ . '. (
+       )  / <<    >> \  (
+        '-._/``  ``\_.-'
+  jgs     __\\\\\\\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
+
+def check_name(name: str) -> bool:
+    if name == "jgsbat" or name in list_cows():
+        return True
+    return False
+
+def move(direction: str) -> None:
+    global player
+
+    directions = {
+        "up": (-1, 0),
+        "down": (1, 0),
+        "left": (0, -1),
+        "right": (0, 1)
+    }
+
+    dx, dy = directions[direction]
+    player.move(dx, dy)
+
+    print(f"Moved to ({player[0]}, {player[1]})")
+
+    for monster in monsters:
+        if monster.pos == player:
+            encounter(player[0], player[1])
+    return
+    
+def encounter(x, y):
+    pos = Position(x, y)
+    for monster in monsters:
+        if monster.pos == pos:
+            if monster.name == "jgsbat":
+                print(cowsay(monster.phrase, cowfile=jgsbat))
+            else:
+                print(cowsay(monster.phrase, cow=monster.name))
+
+def get_and_make_command():
+    try:
+        command_line = input()
+    except EOFError:
+        exit(0)
+
+    if not command_line.strip():
+        return
+
+    command, *args = split(command_line)
+    if command not in commands:
+        print("Invalid command")
+        return
+    
+    if command != "addmon":
+        move(command)
+        return
+    
+    if len(args) != 8:
+        print("Invalid arguments")
+        return
+
+    name = args[0]
+    hp = 0
+    phrase = ''
+    new_pos = Position(0, 0)
+
+    real_name = check_name(name)
+    if not real_name:
+        print("Cannot add unknown monster")
+        return
+
+
+    idx = 1
+    while idx < len(args):
+        if args[idx] == "hello":
+            if idx + 1 >= len(args):
+                print("Invalid arguments")
+                return
+            idx += 1
+            phrase = args[idx]
+
+        elif args[idx] == "hp":
+            if idx + 1 >= len(args):
+                print("Invalid arguments")
+                return
+            idx += 1
+            hp = int(args[idx])
+            if hp < 0:
+                print("Invalid arguments")
+                return
+        
+        elif args[idx] == "coords":
+            if idx + 2 >= len(args):
+                print("Invalid arguments")
+                return
+            idx += 2
+            x, y = int(args[idx - 1]), int(args[idx])
+            if x < 0 or x > 9 or y < 0 or y > 9:
+                print("Invalid arguments")
+                return
+            new_pos = Position(x, y)
+        else:
+            print("Invalid arguments")
+            return
+        idx += 1
+    
+    print(f"Added monster {name} to {new_pos} saying {phrase}")
+
+    monster_replaced = False
+    for i, monster in enumerate(monsters):
+        if monster.pos == new_pos:
+            monsters[i] = Monster(new_pos, name, phrase, hp)
+            monster_replaced = True
+            break
+    
+    if not monster_replaced:
+        monsters.append(Monster(new_pos, name, phrase, hp))
+    else:
+        print("Replaced the old monster")
+    
+def main():
+    print("<<< Welcome to Python-MUD 0.1 >>>")
+
+    while True:
+        get_and_make_command()
+    
+if __name__ == "__main__":
+    main()
